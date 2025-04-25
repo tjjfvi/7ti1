4ae93a0b28895dcfd2747303e66524c9ad626bf7
tjjfvi <t6@t6.fyi>
2025-04-25 18:33:20 -0400

    initial project

---
+++ src/interface.ts
@@ -0,0 +1,103 @@
+import { collapse, expand, Tree, Tree7 } from "./trees.ts";
+
+const nodeSize = 20;
+const nodeSpacing = 10;
+const rowSize = 25;
+const lineWeight = 2.5;
+const treeSpacing = 20;
+
+const treeSvg = document.getElementById("tree")!;
+const tree7Svg = document.getElementById("tree7")!;
+
+let tree: Tree = [];
+let tree7: Tree7 = expand(tree);
+
+function render() {
+  drawTrees(treeSvg, [tree], () => {
+    tree7 = expand(tree);
+    render();
+  });
+  drawTrees(tree7Svg, tree7, () => {
+    tree = collapse(tree7);
+    render();
+  });
+}
+
+render();
+
+function drawTrees(svg: HTMLElement, trees: Tree[], rerender: () => void) {
+  svg.replaceChildren();
+
+  let xPos = 0;
+  let width = 0;
+  let height = 0;
+
+  for (const tree of trees) {
+    drawTree(tree, 0);
+    xPos += treeSpacing;
+  }
+  
+  const pad = nodeSpacing + nodeSize / 2;
+  svg.setAttributeNS(null, "viewBox", `${-pad} ${-pad} ${width + pad * 2} ${height + pad * 2}`);
+  svg.setAttributeNS(null, "width", `${width + pad * 2}`);
+  svg.setAttributeNS(null, "height", `${height + pad * 2}`);
+
+  function drawTree(tree: Tree, yPos: number): number {
+    if (tree.length === 0) {
+      const center = xPos;
+      xPos += nodeSize + nodeSpacing;
+      const node = drawNode(center, yPos);
+      node.setAttributeNS(null, "style", "cursor: pointer")
+      node.addEventListener("click", () => {
+        (tree as Tree[]).push([], []);
+        rerender();
+      });
+      return center;
+    }
+
+    const left = drawTree(tree[0], yPos + rowSize);
+    const right = drawTree(tree[1], yPos + rowSize);
+    const center = (left + right) / 2;
+    drawLine(center, yPos, left, yPos + rowSize);
+    drawLine(center, yPos, right, yPos + rowSize);
+    const node = drawNode(center, yPos);
+    if (tree[0].length === 0 && tree[1].length === 0) {
+      node.setAttributeNS(null, "style", "cursor: pointer")
+      node.addEventListener("click", () => {
+        (tree as Tree).length = 0;
+        rerender();
+      });
+    } else {
+      node.addEventListener("dblclick", () => {
+        (tree as Tree).length = 0;
+        rerender();
+      });
+    }
+    return center;
+  }
+
+  function drawNode(x: number, y: number): SVGCircleElement {
+    width = Math.max(width, x);
+    height = Math.max(height, y);
+    const circle = svgEl("circle") as SVGCircleElement;
+    circle.setAttributeNS(null, "cx", x + "");
+    circle.setAttributeNS(null, "cy", y + "");
+    circle.setAttributeNS(null, "r", nodeSize / 2 + "");
+    svg.appendChild(circle);
+    return circle;
+  }
+
+  function drawLine(x1: number, y1: number, x2: number, y2: number) {
+    const line = svgEl("line") as SVGLineElement;
+    line.setAttributeNS(null, "x1", x1 + "");
+    line.setAttributeNS(null, "y1", y1 + "");
+    line.setAttributeNS(null, "x2", x2 + "");
+    line.setAttributeNS(null, "y2", y2 + "");
+    line.setAttributeNS(null, "stroke-width", lineWeight + "");
+    svg.appendChild(line);
+  }
+}
+
+function svgEl(name: string): SVGElement {
+  return document.createElementNS("http://www.w3.org/2000/svg", name);
+}
---
+++ src/trees.ts
@@ -0,0 +1,69 @@
+
+export type Tree = [] | [Tree, Tree];
+export type Tree7 = [Tree, Tree, Tree, Tree, Tree, Tree, Tree];
+
+type Pat = number | [] | [Pat, Pat];
+type Pat7 = [Pat, Pat, Pat, Pat, Pat, Pat, Pat];
+
+const mapping: [Pat, Pat7][] = [
+  [[], [[], [], [], [], [], [], []]],
+  [[0, []], [[0, []], [], [], [], [], [], []]],
+  [[0, [1, []]], [[0, [1, []]], [], [], [], [], [], []]],
+  [[0, [1, [2, []]]], [0, [1, 2], [], [], [], [], []]],
+  [[0, [1, [2, [3, []]]]], [[0, [1, [2, 3]]], [], [], [], [], [], []]],
+  [[0, [1, [2, [3, [[], []]]]]], [0, 1, [2, 3], [], [], [], []]],
+  [[0, [1, [2, [3, [[4, 5], []]]]]], [0, 1, 2, 3, [4, 5], [], []]],
+  [[0, [1, [2, [3, [4, [[], []]]]]]], [0, 1, 2, [3, 4], [], [], []]],
+  [[0, [1, [2, [3, [4, [[5, 6], []]]]]]], [0, 1, 2, 3, 4, [5, 6], []]],
+  [[0, [1, [2, [3, [4, [5, [6, 7]]]]]]], [0, 1, 2, 3, 4, 5, [6, 7]]],
+];
+
+const vars: Record<number, Tree> = {};
+
+export function expand(tree: Tree): Tree7 {
+  for (const [pat, pat7] of mapping) {
+    if (match(tree, pat)) {
+      return reconstruct7(pat7);
+    }
+  }
+  throw new Error("unreachable");
+}
+
+export function collapse(tree7: Tree7): Tree {
+  for (const [pat, pat7] of mapping) {
+    if (match7(tree7, pat7)) {
+      return reconstruct(pat);
+    }
+  }
+  throw new Error("unreachable");
+}
+
+function match(tree: Tree, pat: Pat): boolean {
+  if (typeof pat === "number") {
+    vars[pat] = tree;
+    return true;
+  } else if (pat.length === 0) {
+    return tree.length === 0;
+  } else {
+    return tree.length === 2 && match(tree[0], pat[0]) &&
+      match(tree[1], pat[1]);
+  }
+}
+
+function reconstruct(pat: Pat): Tree {
+  if (typeof pat === "number") {
+    return vars[pat]!;
+  } else if (pat.length === 0) {
+    return [];
+  } else {
+    return [reconstruct(pat[0]), reconstruct(pat[1])];
+  }
+}
+
+function match7(tree7: Tree7, pat7: Pat7): boolean {
+  return tree7.every((tree, i) => match(tree, pat7[i]!));
+}
+
+function reconstruct7(pat7: Pat7): Tree7 {
+  return pat7.map(reconstruct) as Tree7;
+}
